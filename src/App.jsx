63e6bd001c0ca2e071ae863cea8824442c770971
Tristan Ross <Tristan@Tristans-MacBook-Pro-2.local>
2025-01-24 17:55:25 -0800

    Added click functionality and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import React, {useState} from "react";
-
-import { puppyList } from "./data.js";
+import React, {useState} from 'react';
+import { puppyList } from './data.js';
+import "./App.css";
 
 function App() {
-  console.log(puppyList);
-  const [puppies, setPuppies] =useState(puppyList);
-  return <>
-    <h1>Puppy Pals</h1>
-    {puppies.map((puppy) => (
-      <p key={puppy.id}>{puppy.name}</p>
-    ))}
-  </>;
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPupInfo = puppies.find((puppy) => puppy.id === featPupId);
 
+  return (
+    <>
+      <h1>Puppy Pals</h1>
+      {featPupId && <p className={"id"}>Info for Puppy with Id: {featPupId}</p>}
+      {featPupId && (
+        <div className={"box"}>
+          <h2>{featuredPupInfo.name}</h2>
+          <ul>
+            <li>Age: {featuredPupInfo.age}</li>
+            <li>Email: {featuredPupInfo.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => (
+        <p 
+        key={puppy.id} onClick={() => {
+          setFeatPupId(puppy.id)}}>{puppy.name}</p>
+      ))}
+
+
+    </>
+  );
 }
 
-export default App
+export default App;
